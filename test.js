fa6e0705f5a0028d34fa0b49155e81d1634cc61a
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2016-07-07 10:45:24 -0400

    Untested Log

--- test.js
+++ test.js
@@ -1,9 +1,35 @@
 var Scratch = require('scratch-api');
+var fs = require('fs');
 var bridgeUsers = require('./users.json');
 var statusId = 115628457;
 var serverId = '☁ server1timestamp'
 var username = 'bridgevar';
 var password = 'password';
+var stream = fs.createWriteStream("./log.txt");
+function getDateTime() {
+
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
+
+}
 var createBridges = function(start, end) {
     for (var i=start; i<end; i++) {
         var firstProjectId = bridgeUsers.users[i].firstProjectId;
@@ -15,15 +41,21 @@ var createBridges = function(start, end) {
             user.cloudSession(secondProjectId, function(err, secondcloud) {
                 firstcloud.on('set', function(name, value) {
                     if (name === '☁ ' + firstName) {
+                        var val = value;
                         secondcloud.set('☁ ' + secondName, value);
                     }
                 });
                 secondcloud.on('set', function(name, value) {
                     if (name === '☁ ' + secondName) {
+                        var val = value;
                         firstcloud.set('☁ ' + secondName, value);
                     }
                 });
             });
+        });
+        stream.once('open', function(fd) {
+            stream.write("At " + getDateTime() + " " + firstProjectId + " sent " + secondProjectId + " the value " + val + "\n");
+            stream.end();
         });
     }
 }
