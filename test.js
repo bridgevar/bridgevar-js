a28fa84dd65346e9a9e1c6b769843713f2f9f9c9
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2016-07-16 08:30:48 -0400

    Clean Up

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ var username = 'bridgevar';
 var password = 'password';
 var val = "";
 var createBridges = function(start, end, user) {
-    for (var i=start; i<end; i++) {
+    for (var i = start; i < end; i++) {
         var firstProjectId = bridgeUsers.users[i].firstProjectId;
         var secondProjectId = bridgeUsers.users[i].secondProjectId;
         var firstName = bridgeUsers.users[i].firstName;
@@ -14,30 +14,54 @@ var createBridges = function(start, end, user) {
         var oneWay = bridgeUsers.users[i].oneway;
         var myi = i;
         user.cloudSession(firstProjectId, function(err, firstcloud) {
-            user.cloudSession(secondProjectId, function(err, secondcloud) {
+            user.cloudSession(secondProjectId, function(err,
+                secondcloud) {
                 firstcloud.on('set', function(name, value) {
                     if (name === '☁ ' + firstName) {
-                        secondcloud.set('☁ ' + secondName, value);
-                        console.log("At " + (new Date()).toUTCstring() + " " + firstProjectId + " sent " + secondProjectId + " the value " + value.toString());
+                        secondcloud.set('☁ ' +
+                            secondName, value);
+                        console.log("At " + (new Date())
+                            .toUTCstring() +
+                            " " +
+                            firstProjectId +
+                            " sent " +
+                            secondProjectId +
+                            " the value " +
+                            value.toString());
                     }
                 });
-                if(!oneWay){
-                    secondcloud.on('set', function(name, value) {
-                        if (name === '☁ ' + secondName) {
-                            firstcloud.set('☁ ' + firstName, value);
-                            console.log("At " + (new Date()).toUTCstring() + " " + firstProjectId + " sent " + secondProjectId + " the value " + value.toString());
+                if (!oneWay) {
+                    secondcloud.on('set', function(name,
+                        value) {
+                        if (name === '☁ ' +
+                            secondName) {
+                            firstcloud.set('☁ ' +
+                                firstName,
+                                value);
+                            console.log("At " + (
+                                    new Date())
+                                .toUTCstring() +
+                                " " +
+                                firstProjectId +
+                                " sent " +
+                                secondProjectId +
+                                " the value " +
+                                value.toString()
+                            );
                         }
-					});
-				}
+                    });
+                }
             });
         });
-        
     }
 }
 Scratch.UserSession.create(username, password, function(err, user) {
     user.cloudSession(statusId, function(err, statusSession) {
-        var pulse = function(session) {session.set(serverId, Date.now()); setTimeout(pulse, 50000, session)};
+        var pulse = function(session) {
+            session.set(serverId, Date.now());
+            setTimeout(pulse, 50000, session)
+        };
         pulse(statusSession);
-        createBridges(0,bridgeUsers.length,user)
+        createBridges(0, bridgeUsers.length, user)
     });
 });
