1f276c07a3b7605b7f9218ea6da9aee30cb1614b
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2016-07-20 12:57:19 -0400

    No max

--- runwithforever.js
+++ runwithforever.js
@@ -2,12 +2,11 @@
 
   var child = new (forever.Monitor)('bridge.js', {
     silent: true,
-    'max': 1000000000,  
     args: []
   });
 
   child.on('exit', function () {
-    console.log('bridge.js has exited after 1000000000 restarts.');
+    console.log('bridge.js has exited.');//It will never go to this probebly.
   });
 
   child.start();
